d57742f35208b3efcae7a0f7a1de605e8dbf8842
usha1 <usha123@gmail.com>
2018-10-30 14:46:06 +0530

    added 2 new chars

--- Samplegithub/src/Names.java
+++ Samplegithub/src/Names.java
@@ -3,10 +3,11 @@ public class Names {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  String[] names = {"usha","abi","karthik","vignesh"};
+  String[] names = {"usha","abi","karthik","vignesh","aaaa","bbbb"};
   for(int i=0; i<names.length;i++)
   {
 	  System.out.println("hi "+names[i]);
+	  System.out.println("How are you doing tdy?");
   }
 	}
 
