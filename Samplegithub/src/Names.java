68004662b334c9f33c5f076a17c9ab04c83798d2
usha <usha123@gmail.com>
2018-10-30 01:38:09 -0700

    committed names class

---
+++ Samplegithub/src/Names.java
@@ -0,0 +1,13 @@
+
+public class Names {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  String[] names = {"usha","abi","karthik","vignesh"};
+  for(int i=0; i<names.length;i++)
+  {
+	  System.out.println("hi "+names[i]);
+  }
+	}
+
+}
